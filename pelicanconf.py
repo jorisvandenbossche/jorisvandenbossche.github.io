d98b1430f7e84b9978347bd16f41060f3e87c095
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-09-19 22:58:00 +0200

    add pelican-themes submodule

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Joris Van den Bossche'
 SITENAME = 'Joris Van den Bossche'
-SITEURL = ''
+SITEURL = 'https://jorisvandenbossche.github.io'
 
 PATH = 'content'
 
@@ -19,17 +19,24 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Theme
+THEME = "pelican-themes/pelican-sober"
+
+PELICAN_SOBER_ABOUT = "Pandas core developer. Working at the Paris-Saclay Center for Data Science"
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/jorisvandenbossche/'),
+          ('twitter', 'https://twitter.com/jorisvdbossche'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+LOAD_CONTENT_CACHE = False
